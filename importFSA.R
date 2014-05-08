7ef0bb4a087b8db0e851f3f32645855bcccdd0b8
SamBuckberry <sambuckberry@gmail.com>
2014-05-08 16:53:51 +0930

    initial commit with sample files

---
+++ importFSA.R
@@ -0,0 +1,15 @@
+install.packages("seqinr")
+library(seqinr)
+
+# Read in .fsa file
+dat <- read.abif(filename="A01_913_HpaII_CA_AAG.fsa")
+head(dat)
+names(dat)
+
+
+# Extract the data for the FAM lables
+
+# Extract the data for the ladder
+
+# Generate a plot of the data
+
